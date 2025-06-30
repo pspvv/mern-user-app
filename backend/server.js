44c130e9a461cefa0771c9990f9e02af374d0fa1
V V SATYA PAVAN SANDEEP PENTAPATI <107297908+pspvv@users.noreply.github.com>
2025-06-30 11:55:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({origin:'https://mern-user-app-1.onrender.com'}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
